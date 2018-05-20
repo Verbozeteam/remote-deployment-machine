6f686ae30d3825b38c8e0c67f8b343d079b0719c
Hasan-Jawaheri <hasan.aljawaheri@gmail.com>
2018-05-20 16:09:53 +0300

    small modification to authenticate()

--- connection/communicator.py
+++ connection/communicator.py
@@ -23,36 +23,45 @@ class Communicator:
         return os.uname().nodename
 
     def authenticate(self):
-        auth_info = {
-            'username': AUTH_CONFIG.USERNAME,
-            'password': AUTH_CONFIG.PASSWORD,
-            'machine': self.get_machine_info()
-        }
-        if auth_info['username'] == '':
-            auth_info['username'] = input('Username: ')
-        if auth_info['password'] == '':
-            auth_info['password'] = getpass.getpass('Password: ')
+        n_attemps = 0
+        while n_attemps < 3:
+            n_attemps += 1
+            auth_info = {
+                'username': AUTH_CONFIG.USERNAME,
+                'password': AUTH_CONFIG.PASSWORD,
+                'machine': self.get_machine_info()
+            }
+            if auth_info['username'] == '':
+                auth_info['username'] = input('Username: ')
+            if auth_info['password'] == '':
+                auth_info['password'] = getpass.getpass('Password: ')
 
-        print('Authenticating...')
+            print('Authenticating...')
 
-        if CONFIG.NOT_SECURE:
-            url = 'http://'
-        else:
-            url = 'https://'
-        url += CONFIG.BASE_URL + '/api/token-auth/'
+            if CONFIG.NOT_SECURE:
+                url = 'http://'
+            else:
+                url = 'https://'
+            url += CONFIG.BASE_URL + '/api/token-auth/'
 
-        r = requests.post(url, auth_info)
+            r = requests.post(url, auth_info)
 
-        if r.status_code != 200:
-            print('Authentication error - try again')
-            self.authenticate()
+            if r.status_code != requests.codes['ok']:
+                print('Authentication error - try again (status {})'.format(r.status_code))
+                continue
 
-        elif not 'rdm_ws_token' in r.json():
-            print('You do not have the right permissions to use this tool')
+            elif not 'rdm_ws_token' in r.json():
+                print('You do not have the right permissions to use this tool')
+                break
+
+            else:
+                self.token = r.json()['rdm_ws_token']
+                break
+        if not self.token:
+            print ('Failed to authenticate')
             sys.exit(0)
 
-        else:
-            self.token = r.json()['rdm_ws_token']
+        print ('Authentication successful')
 
     def run(self):
         self.thread = threading.Thread(target=self.initialize_websocket)
